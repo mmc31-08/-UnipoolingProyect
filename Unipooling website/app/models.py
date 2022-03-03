21fc12fab837068be906007e8fe82dfff941364a
mmc31-08 <mateo-31-cadavid@outlook.com>
2022-03-03 12:35:11 -0500

    Pagina inicial

---
+++ Unipooling website/app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class usuario(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=30)
+    universidad=models.CharField(max_length=20)
+    correo=models.EmailField()
+    contraseña=models.CharField(max_length=15)  
+
+#nombre, universidad, correo, contrasena
+
